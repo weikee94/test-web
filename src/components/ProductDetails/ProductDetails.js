dd2cf0f5f4cc831a5a21684c30a3a8eb280e5eed
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2018-12-13 16:43:50 +0800

    updated for edit purpose

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -7,24 +7,60 @@ class ProductDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      text: ""
     };
   }
   componentDidMount() {
     axios.get(`${rootAPI}todos/${this.props.match.params.id}`).then(res => {
       const data = res.data.todo;
       this.setState({
-        data
+        data,
+        text: data.text
       });
     });
   }
+
+  changeHandler = e => {
+    let name, obj;
+    name = e.target.name;
+    this.setState(((obj = {}), (obj["" + name] = e.target.value), obj));
+    console.log("updated valud: ", this.state.text);
+  };
+
+  updateHandler = () => {
+    axios
+      .patch(`${rootAPI}todos/${this.props.match.params.id}`, {
+        text: this.state.text
+      })
+      .then(success => {
+        console.log(success);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
-    console.log(this.state);
     return (
       <div>
         {this.state.data !== null ? this.state.data.text : null}
         <div>
-          <button className="btn btn-primary">update</button>
+          <input
+            name="text"
+            type="text"
+            className="form-control"
+            value={this.state.text}
+            onChange={e => this.changeHandler(e)}
+          />
+        </div>
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => this.updateHandler()}
+          >
+            update
+          </button>
         </div>
       </div>
     );
